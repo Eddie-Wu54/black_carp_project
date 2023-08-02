2b3fa6076f40baf84e82a6d3652ea28de0519b28
Eddie-Wu54 <e54wu@uwaterloo.ca>
2023-08-02 17:24:37 -0400

    add subsampling to the black carp and asian carp model selection.

--- BLACK asian carp model selection.R
+++ BLACK asian carp model selection.R
@@ -1,14 +1,18 @@
-#' This script is used to compare the difference models for Asian carp AAM under
-#' two different conditions. We use:
+#' This script is used to compare the four difference models for establishing
+#' the relationship between AAM and temperature + condition for black carp and
+#' Asian carp.
 #' 
+#' We use:
+#'
 #' 1. Linear addictive model  2. Interaction model
-#' 3. Simple linear model  4. ??? midel
+#' 3. Simple linear model  4. Grouped-specific model
 #' 
 #' on:
 #' 
 #' 1. Annual temperature  2. Cold temperature
 #' 
-#' We also check the Cook's distance to determine if outliers needs to be removed.
+#' We also conducted the same AIC analyses for all Asian carp species. But for
+#' Asian carp, the data is subsampled.
 
 
 library(ggplot2)
@@ -33,58 +37,12 @@ Big.sil <- rbind(Bighead, Silver) # combine the two groups
 
 
 
-#### Asian carp all species ####
+#### Black carp (does not subsample) ####
 
-asian.carp.clean <- asian.carp %>% 
-  filter(Condition %in% c("natural", "artificial"))
-
-## Linear addictive models (same slope, different intercept)
-
-asian.linear <- lm(log(AAM)~ColdTemp+Condition, data = asian.carp.clean)
-anova(asian.linear)
-summary(asian.linear)
-autoplot(asian.linear)
-
-plot(asian.linear, which = 5)
-
-
-
-
-## Interaction models (ANCOVA: different slope, different intercept)
-asian.int <- lm(log(AAM)~ColdTemp*Condition, data = asian.carp.clean)
-anova(asian.int)
-summary(asian.int)
-autoplot(asian.int)
-
-plot(asian.int, which = 5)
-
-
-## Simple linear model (same slope, same intercept)
-
-asian.simple <- lm(log(AAM)~ColdTemp, data = asian.carp.clean)
-anova(asian.simple)
-summary(asian.simple)
-autoplot(asian.simple)
-
-plot(asian.simple, which = 5)
-
-## Compare the AIC scores
-AIC(asian.linear)
-AIC(asian.int)
-AIC(asian.simple)
-
-# Check the Cook's distance that are three times larger than the mean
-cooksD <- cooks.distance(asian.simple)
-influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
-influential
-
-
-
-#### Black carp ####
-
-Black.clean <- Black %>% filter(!row_number() == 15) %>% filter(sex != "male") %>% 
+Black.clean <- Black %>% filter(!row_number() == 5) %>% filter(sex != "male") %>% 
   filter(!condition == "na")
 
+# OPTIONAL
 Black.clean <- Black.clean[Black.clean$AAM != 11,]
 Black.clean <- Black.clean[Black.clean$AAM != 4,]
 Black.clean <- Black.clean[Black.clean$location != "Heilong Jiang"]
@@ -124,90 +82,179 @@ plot(black.int, which = 5)
 
 ## Compare the AIC scores
 AIC(black.simple, black.linear, black.int, black.group)
-    
-    
+
+
 ## Outlier check
 hist(log(Black.clean$AAM))
 mean(log(Black.clean$AAM)) + 3 * sd(log(Black.clean$AAM))
 sd(log(Black.clean$AAM))
 
 
-#### Grass carp ####
+
+#### Asian carp all species (subsample 1000 times) ####
+
+## Look at the spatial codes for the current asian carp data
+asian.carp.clean <- asian.carp %>% 
+  filter(Condition %in% c("natural", "artificial"))
+
+table(asian.carp.clean$Code)
+
+
+## Simple linear model
+asian.linear.results <- matrix(NA,1000,1)
+for(i in 1:1000){
+  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp, data = sub)
+  asian.linear.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
+## Linear addictive models (same slope, different intercept)
+asian.add.results <- matrix(NA,1000,1)
+for(i in 1:1000){
+  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp+Condition, data = sub)
+  asian.add.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
+## Interaction models (different slope, same intercept)
+asian.int.results <- matrix(NA,1000,1)
+for(i in 1:1000){
+  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp:Condition, data = sub)
+  asian.int.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
+## Grouped-specific model (different slope, different intercept)
+asian.group.results <- matrix(NA,1000,1)
+for(i in 1:1000){
+  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
+  asian.group.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
+## Compare the AIC results
+mean(unique(asian.linear.results[,1]))
+mean(unique(asian.add.results[,1]))
+mean(unique(asian.int.results[,1]))
+mean(unique(asian.group.results[,1]))
+
+# Do I compare the mean? Or other values?
+# Do I need to do each individual asian carp species?
+
+
+
+# Check the Cook's distance that are three times larger than the mean
+cooksD <- cooks.distance(asian.simple)
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
+influential
+
+
+
+#### Grass carp (subsample 100 times) ####
 
 Grass.clean <- Grass %>% 
   filter(Condition %in% c("natural", "artificial"))
 
+table(Grass.clean$Code)
+
+
+## Simple linear model
+grass.linear.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Grass.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp, data = sub)
+  grass.linear.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
 ## Linear addictive models (same slope, different intercept)
-
-grass.linear <- lm(log(AAM)~ColdTemp+Condition, data = Grass.clean)
-anova(grass.linear)
-summary(grass.linear)
-autoplot(grass.linear)
-
-plot(grass.linear, which = 5)
-
-
-## Interaction models (ANCOVA: different slope, different intercept)
-grass.int <- lm(log(AAM)~ColdTemp*Condition, data = Grass.clean)
-anova(grass.int)
-summary(grass.int)
-autoplot(grass.int)
-
-plot(grass.int, which = 5)
+grass.add.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Grass.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp+Condition, data = sub)
+  grass.add.results[i,1]<-as.numeric(AIC(reg))
+}
 
 
-## Simple linear model (same slope, same intercept)
+## Interaction models (different slope, same intercept)
+grass.int.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Grass.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp:Condition, data = sub)
+  grass.int.results[i,1]<-as.numeric(AIC(reg))
+}
 
-grass.simple <- lm(log(AAM)~ColdTemp, data = Grass.clean)
-anova(grass.simple)
-summary(grass.simple)
-autoplot(grass.simple)
 
-plot(grass.simple, which = 5)
+## Grouped-specific model (different slope, different intercept)
+grass.group.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Grass.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
+  grass.group.results[i,1]<-as.numeric(AIC(reg))
+}
 
 ## Compare the AIC scores
-AIC(grass.linear)
-AIC(grass.int)
-AIC(grass.simple)
+mean(unique(grass.linear.results[,1]))
+mean(unique(grass.add.results[,1]))
+mean(unique(grass.int.results[,1]))
+mean(unique(grass.group.results[,1]))
 
 
 
-#### Bighead and Silver carp ####
+#### Bighead and Silver carp (subsample 100 times) ####
 
 Big.sil.clean <- Big.sil %>% 
   filter(Condition %in% c("natural", "artificial"))
 
+table(Big.sil.clean$Code)
+
+
+## Simple linear model
+bs.linear.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Big.sil.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp, data = sub)
+  bs.linear.results[i,1]<-as.numeric(AIC(reg))
+}
+
+
 ## Linear addictive models (same slope, different intercept)
-
-bs.linear <- lm(log(AAM)~ColdTemp+Condition, data = Big.sil.clean)
-anova(bs.linear)
-summary(bs.linear)
-autoplot(bs.linear)
-
-plot(bs.linear, which = 5)
-
-
-## Interaction models (ANCOVA: different slope, different intercept)
-bs.int <- lm(log(AAM)~ColdTemp*Condition, data = Big.sil.clean)
-anova(bs.int)
-summary(bs.int)
-autoplot(bs.int)
-
-plot(bs.int, which = 5)
+bs.add.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Big.sil.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp+Condition, data = sub)
+  bs.add.results[i,1]<-as.numeric(AIC(reg))
+}
 
 
-## Simple linear model (same slope, same intercept)
+## Interaction models (different slope, same intercept)
+bs.int.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Big.sil.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp:Condition, data = sub)
+  bs.int.results[i,1]<-as.numeric(AIC(reg))
+}
 
-bs.simple <- lm(log(AAM)~ColdTemp, data = Big.sil.clean)
-anova(bs.simple)
-summary(bs.simple)
-autoplot(bs.simple)
 
-plot(bs.simple, which = 5)
+## Grouped-specific model (different slope, different intercept)
+bs.group.results <- matrix(NA,100,1)
+for(i in 1:100){
+  sub <- Big.sil.clean %>% group_by(Code) %>% sample_n(size=1)
+  reg <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
+  bs.group.results[i,1]<-as.numeric(AIC(reg))
+}
 
 ## Compare the AIC scores
-AIC(bs.linear)
-AIC(bs.int)
-AIC(bs.simple)
+mean(unique(bs.linear.results[,1]))
+mean(unique(bs.add.results[,1]))
+mean(unique(bs.int.results[,1]))
+mean(unique(bs.group.results[,1]))
+
+
+
 
 
