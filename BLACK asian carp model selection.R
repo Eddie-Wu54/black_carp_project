0b8d7a2af910640c53c5431c0e1f1cd061a9d68e
Eddie-Wu54 <e54wu@uwaterloo.ca>
2023-09-20 14:08:00 -0400

    Add new slope analyses for black carp and asian carp. Rearrange the sections to make the structure more clear.

--- BLACK asian carp model selection.R
+++ BLACK asian carp model selection.R
@@ -17,7 +17,7 @@
 
 library(ggplot2)
 library(ggfortify)
-library(dplyr)
+library(tidyverse)
 
 
 ## Import data
@@ -54,7 +54,7 @@ anova(black.simple)
 summary(black.simple)
 autoplot(black.simple) # did not meet??
 
-plot(black.simple, which = 5)
+plot(black.simple, which = 2)
 
 
 ## Linear addictive models (same slope, different intercept)
@@ -98,52 +98,115 @@ asian.carp.clean <- asian.carp %>%
   filter(Condition %in% c("natural", "artificial"))
 
 table(asian.carp.clean$Code)
+table(asian.carp.clean$Code_Str)
 
 
-## Simple linear model
-asian.linear.results <- matrix(NA,1000,1)
+## Subsampling for 1000 times (define all four models in one iteration!)
+# Create the matrices to store the results
+asian.linear.results <- matrix(NA,1000,4)
+asian.add.results <- matrix(NA,1000,4)
+asian.int.results <- matrix(NA,1000,4)
+asian.group.results <- matrix(NA,1000,4)
+
+
+# For 1000 iterations
 for(i in 1:1000){
-  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
-  reg <- lm(log(AAM)~AnnualTemp, data = sub)
-  asian.linear.results[i,1]<-as.numeric(AIC(reg))
+  sub <- asian.carp.clean %>% group_by(Code_Str) %>% sample_n(size=1)
+  
+  # annual
+  reg.linear.annual <- lm(log(AAM)~AnnualTemp, data = sub)
+  reg.add.annual <- lm(log(AAM)~AnnualTemp+Condition, data = sub)
+  reg.int.annual <- lm(log(AAM)~AnnualTemp:Condition, data = sub)
+  reg.group.annual <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
+  
+  # AICs for annual
+  asian.linear.results[i,1]<-as.numeric(AICc(reg.linear.annual))
+  asian.add.results[i,1]<-as.numeric(AICc(reg.add.annual))
+  asian.int.results[i,1]<-as.numeric(AICc(reg.int.annual))
+  asian.group.results[i,1]<-as.numeric(AICc(reg.group.annual))
+  
+  # R2 for annual
+  asian.linear.results[i,2]<-summary(reg.linear.annual)$adj.r.squared
+  asian.add.results[i,2]<-summary(reg.add.annual)$adj.r.squared
+  asian.int.results[i,2]<-summary(reg.int.annual)$adj.r.squared
+  asian.group.results[i,2]<-summary(reg.group.annual)$adj.r.squared
+  
+  # cold
+  reg.linear.cold <- lm(log(AAM)~ColdTemp, data = sub)
+  reg.add.cold <- lm(log(AAM)~ColdTemp+Condition, data = sub)
+  reg.int.cold <- lm(log(AAM)~ColdTemp:Condition, data = sub)
+  reg.group.cold <- lm(log(AAM)~ColdTemp*Condition, data = sub)
+  
+  # AICs for cold
+  asian.linear.results[i,3]<-as.numeric(AICc(reg.linear.cold))
+  asian.add.results[i,3]<-as.numeric(AICc(reg.add.cold))
+  asian.int.results[i,3]<-as.numeric(AICc(reg.int.cold))
+  asian.group.results[i,3]<-as.numeric(AICc(reg.group.cold))
+  
+  # R2 for cold
+  asian.linear.results[i,4]<-summary(reg.linear.cold)$adj.r.squared
+  asian.add.results[i,4]<-summary(reg.add.cold)$adj.r.squared
+  asian.int.results[i,4]<-summary(reg.int.cold)$adj.r.squared
+  asian.group.results[i,4]<-summary(reg.group.cold)$adj.r.squared
 }
 
 
-## Linear addictive models (same slope, different intercept)
-asian.add.results <- matrix(NA,1000,1)
-for(i in 1:1000){
-  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
-  reg <- lm(log(AAM)~AnnualTemp+Condition, data = sub)
-  asian.add.results[i,1]<-as.numeric(AIC(reg))
-}
+## R^2 values for the four models
+# annual
+r2annual <- data.frame(
+  Model = c("Simple linear", "Linear additive", "Interaction", "Grouped"),
+  R2 = c(mean(unique(asian.linear.results[,2])),
+         mean(unique(asian.add.results[,2])),
+         mean(unique(asian.int.results[,2])),
+         mean(unique(asian.group.results[,2])))
+)
+
+# cold
+r2cold <- data.frame(
+  Model = c("Simple linear", "Linear additive", "Interaction", "Grouped"),
+  R2 = c(mean(unique(asian.linear.results[,4])),
+         mean(unique(asian.add.results[,4])),
+         mean(unique(asian.int.results[,4])),
+         mean(unique(asian.group.results[,4])))
+)
 
 
-## Interaction models (different slope, same intercept)
-asian.int.results <- matrix(NA,1000,1)
-for(i in 1:1000){
-  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
-  reg <- lm(log(AAM)~AnnualTemp:Condition, data = sub)
-  asian.int.results[i,1]<-as.numeric(AIC(reg))
-}
-
-
-## Grouped-specific model (different slope, different intercept)
-asian.group.results <- matrix(NA,1000,1)
-for(i in 1:1000){
-  sub <- asian.carp.clean %>% group_by(Code) %>% sample_n(size=1)
-  reg <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
-  asian.group.results[i,1]<-as.numeric(AIC(reg))
-}
-
-
-## Compare the AIC results
+## AIC values - below is only for one temperature metric
+# Quick average of AICs
 mean(unique(asian.linear.results[,1]))
 mean(unique(asian.add.results[,1]))
 mean(unique(asian.int.results[,1]))
 mean(unique(asian.group.results[,1]))
 
-# Do I compare the mean? Or other values?
-# Do I need to do each individual asian carp species?
+# Calculate the differences of AIC values
+aic.asian <- matrix(NA,1000,3) # store the differences in AIC values
+aic.asian[,1] <- asian.add.results[,1] - asian.linear.results[,1]
+aic.asian[,2] <- asian.int.results[,1] - asian.linear.results[,1]
+aic.asian[,3] <- asian.group.results[,1] - asian.linear.results[,1]
+
+
+# Look at the distribution of differences
+# Create a data frame
+data <- as.data.frame(aic.asian)
+colnames(data) <- c("additive-linear","interation-linear","group-linear")
+
+# Convert to long data format
+data_long <- data %>%
+  pivot_longer(cols = everything(), names_to = "Group", values_to = "Value")
+
+# Define the desired order of groups
+desired_order <- c("additive-linear","interation-linear","group-linear")
+
+# Convert "Group" to a factor with desired order
+data_long$Group <- factor(data_long$Group, levels = desired_order)
+
+# Violin plot
+ggplot(data_long, aes(x = Group, y = Value, fill = Group))+
+  geom_violin()+
+  labs(title = "Differences of AIC scores among models, using simple linear model as base. Annual Temp")+
+  theme_bw()
+
+
 
 
 
@@ -197,6 +260,30 @@ for(i in 1:100){
   grass.group.results[i,1]<-as.numeric(AIC(reg))
 }
 
+
+## Look at the distribution of the AIC scores
+# Create a data frame
+data <- as.data.frame(cbind(grass.linear.results, grass.add.results,
+                            grass.int.results, grass.group.results))
+colnames(data) <- c("linear", "additive", "interaction", "grouped")
+
+# Convert to long data format
+data_long <- data %>%
+  pivot_longer(cols = everything(), names_to = "Group", values_to = "Value")
+
+# Define the desired order of groups
+desired_order <- c("linear", "additive", "interaction", "grouped")
+
+# Convert "Group" to a factor with desired order
+data_long$Group <- factor(data_long$Group, levels = desired_order)
+
+# Violin plot
+ggplot(data_long, aes(x = Group, y = Value, fill = Group))+
+  geom_violin()+
+  labs(title = "Grass carp 100 iterations")+
+  theme_bw()
+
+
 ## Compare the AIC scores
 mean(unique(grass.linear.results[,1]))
 mean(unique(grass.add.results[,1]))
@@ -247,6 +334,30 @@ for(i in 1:100){
   reg <- lm(log(AAM)~AnnualTemp*Condition, data = sub)
   bs.group.results[i,1]<-as.numeric(AIC(reg))
 }
+
+
+## Look at the distribution of the AIC scores
+# Create a data frame
+data <- as.data.frame(cbind(bs.linear.results, bs.add.results,
+                            bs.int.results, bs.group.results))
+colnames(data) <- c("linear", "additive", "interaction", "grouped")
+
+# Convert to long data format
+data_long <- data %>%
+  pivot_longer(cols = everything(), names_to = "Group", values_to = "Value")
+
+# Define the desired order of groups
+desired_order <- c("linear", "additive", "interaction", "grouped")
+
+# Convert "Group" to a factor with desired order
+data_long$Group <- factor(data_long$Group, levels = desired_order)
+
+# Violin plot
+ggplot(data_long, aes(x = Group, y = Value, fill = Group))+
+  geom_violin()+
+  labs(title = "Bighead and silver carp 100 iterations")+
+  theme_bw()
+
 
 ## Compare the AIC scores
 mean(unique(bs.linear.results[,1]))
