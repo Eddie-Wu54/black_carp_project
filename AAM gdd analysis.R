69d7057d3f2bd1c729214d8a03626cf31dbc7341
Eddie-Wu54 <e54wu@uwaterloo.ca>
2023-10-12 14:07:29 -0400

    Clean the codes.

--- AAM gdd analysis.R
+++ AAM gdd analysis.R
@@ -1,8 +1,10 @@
 #' This script is used to calculate the relationship between black carp age at
-#' maturity and growing degree day
+#' maturity and growing degree day (different bases). We want to determine if
+#' the relationship would be different using different bases.
 
 library(ggplot2)
 library(dplyr)
+
 
 #### Import data ####
 
@@ -10,24 +12,15 @@ library(dplyr)
 carp <- read.csv("eddie_carp_new.csv")
 
 # Seperate by sex
-carp.f <- carp %>% filter(!row_number() == 15) %>% filter(sex != "male")
-carp.m <- carp %>% filter(!row_number() == 15) %>% filter(sex != "female")
+carp.f <- carp %>% filter(!row_number() == 5) %>% 
+  filter(sex != "male") # remove all males
+
+carp.m <- carp %>% filter(!row_number() == 5) %>% 
+  filter(sex != "female")
+# here we still keep the SU data point
+
 
 #### Relationship between growing degree day base 0 and AAM ####
-
-## Both sexes:
-# Regression:
-age.gdd <- lm(log(carp$AAM)~carp$average_gdd_0)
-summary(age.gdd)
-plot(age.gdd)
-
-# Plot the AMM verse gdd_0:
-ggplot(carp, mapping = aes(average_gdd_0, AAM))+
-  geom_point(aes(colour = sex))+
-  geom_smooth(method = "lm")+
-  labs(x = "Growing degree day", y = "Age at maturity")+
-  theme_bw()
-
 
 ## For females only:
 age.gdd.f <- lm(log(carp.f$AAM)~carp.f$average_gdd_0)
@@ -41,6 +34,7 @@ ggplot(carp.f, mapping = aes(average_gdd_0, log(AAM)))+
        title = "Female, Base 0")+
   theme_classic(base_size = 16)
 
+
 ## For males only:
 age.gdd.m <- lm(log(carp.m$AAM)~carp.m$average_gdd_0)
 summary(age.gdd.m)
@@ -49,8 +43,10 @@ plot(age.gdd)
 ggplot(carp.m, mapping = aes(average_gdd_0, log(AAM)))+
   geom_point()+
   geom_smooth(method = "lm")+
-  labs(x = "Growing degree day", y = "Age at maturity")+
+  labs(x = "Growing degree day", y = "Age at maturity",
+       title = "Male, Base 0")+
   theme_classic(base_size = 16)
+
 
 
 #### Relationship between growing degree day base 5 and AAM ####
@@ -76,7 +72,7 @@ ggplot(carp.m, mapping = aes(average_gdd_5, log(AAM)))+
   geom_point()+
   geom_smooth(method = "lm")+
   labs(x = "Growing degree day base 5", y = "Age at maturity",
-       title = "Male")+
+       title = "Male, Base 5")+
   theme_classic(base_size = 16)
 
 
@@ -103,5 +99,5 @@ ggplot(carp.m, mapping = aes(average_gdd_10, log(AAM)))+
   geom_point()+
   geom_smooth(method = "lm")+
   labs(x = "Growing degree day base 10", y = "Age at maturity",
-       title = "Male")+
+       title = "Male, Base 10")+
   theme_classic(base_size = 16)
