080a22d59c56b7a9d7bb30217e23979f479aea25
Eddie-Wu54 <e54wu@uwaterloo.ca>
2023-10-12 14:17:08 -0400

    Add title.

--- AAM temperature plots.R
+++ AAM temperature plots.R
@@ -1,6 +1,11 @@
+#' This script plots black carp AAM against annual mean temperature with different
+#' conditions (natural, artificial, combined).
+
+
 library(ggplot2)
 library(ggpubr)
 library(dplyr)
+
 
 # Import the data
 carp <- read.csv("eddie_carp_new.csv")
@@ -76,15 +81,3 @@ ggsave("images/aam latitude asian.jpeg",
 ggsave("images/aam temperatures conditions.jpeg",
        width = 8.5, height = 3.8, dpi = 300)
 
-
-
-
-ggplot(carp.f, mapping = aes(AnnualTemp, AAM, color = condition))+
-  geom_point()+
-  geom_smooth(carp.f.n, mapping = aes(AnnualTemp, AAM), method = "lm", se = T)+
-  geom_smooth(carp.f.a, mapping = aes(AnnualTemp, AAM), method = "lm", se = T)+
-  labs(x = "Annual Mean Temperature", y = "Age at Maturity",
-       title = "(b). Natural and artificial conditions for females")+
-  theme_classic(base_size = 16)+
-  theme(legend.position = c(0.85,0.9),legend.title=element_blank())+
-  scale_color_manual(values = c("artificial" = "red", "natural" = "blue"))
